5980a6aa01951e10ba121a4dd3c6ae77fd3ea22c
190103371 <78577965+190103371@users.noreply.github.com>
2021-02-08 20:33:42 +0600

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,14 @@
+function load(){
+    $("#createID").hide();
+    document.getElementById("accID").style.marginLeft="370px";
+
+}
+function Create(){
+    $("#createID").show();
+    $("#accID").hide();
+    document.getElementById("createID").style.marginLeft="-290px";
+    document.getElementById("createID").style.marginTop="200px";
+    document.getElementById("logoID").style.marginBottom="50px";
+
+
+}
